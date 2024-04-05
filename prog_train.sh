24d1496e3b3af62db832a8ca0bfa69c8f53f53e3
weikang <100151840+weikang-zeng@users.noreply.github.com>
2024-04-05 13:09:46 +0200

    Add files via upload
    For the training script

---
+++ prog_train.sh
@@ -0,0 +1,7 @@
+#OAR -n Project_MInd
+#OAR -l /nodes=2/core=16,walltime=00:08:00
+#OAR --stdout PMind.out
+#OAR --stderr PMind.err
+#OAR --project pr-mind
+
+java -jav EvoAgentZENG.jar ./Minds/T7/Tasks/learningTasks/learnAvoid.simbatch
