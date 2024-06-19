f1c8e4cf46076734ddf20bb3dd67184e9ebe1684
Weikang Zeng Weikang.Zeng@univ-grenoble-alpes.fr <zengw@f-dahu.u-ga.fr>
2024-06-19 13:02:32 +0200

    push to save

--- tesh.sh
+++ tesh.sh
@@ -1,9 +1,9 @@
 #OAR -n Project_PMind
 #OAR --stderr PMind.err
 #OAR --stdout PMind.out
-#OAR -l /core=16,walltime=00:05:30
+#OAR -l /core=16,walltime=08:00:00
 #OAR --project pr-mind
 
 source /applis/site/guix-start.sh
 java -version
-java -jar EvoAgentZENG.jar -nog ./Minds/T7/Tasks/learningTasks/learnAvoid.simbatch
+java -jar EvoAgentZENG.jar -nog ./Minds/T7/Tasks/learningTasks/learnGTO_NoObs.simbatch
